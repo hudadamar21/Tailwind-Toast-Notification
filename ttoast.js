9d50bbb39f022d4446d00767edb7d230c0d2402d
hudadamar21 <hudadamar21@gmail.com>
2020-08-30 14:59:41 +0700

    change type to mode

--- ttoast.js
+++ ttoast.js
@@ -1,7 +1,7 @@
-function TToast (message,type = 'success',costumize = {iconSize : '',roundedClass : '',textClass : '',duration: null,animation: ''}){
+function TToast (message,mode = 'success',costumize = {iconSize : '',roundedClass : '',textClass : '',duration: null,animation: ''}){
   const toast = document.querySelector('.taost');
   let color = '',icon = ''
-  switch (type) {
+  switch (mode) {
     case 'success':
       color = 'green'
       icon = `<svg width="${costumize.iconSize || '1.8em'}" height="${costumize.iconSize || '1.8em'}" viewBox="0 0 16 16" class="bi bi-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/></svg>`
@@ -19,8 +19,8 @@ function TToast (message,type = 'success',costumize = {iconSize : '',roundedClas
       icon = `<svg width="${costumize.iconSize || '1.8em'}" height="${costumize.iconSize || '1.8em'}" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/><path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/></svg>`
       break;
     default: break;}  
-  let html = `<div class="flex items-center bg-${color}-${type == 'info' || type == 'warning' ? '400' : '500'} border-l-4 border-${color}-700 py-2 px-3 shadow-md mb-2 ${costumize.roundedClass} ${costumize.animation}"><div class="text-${color}-${color == 'info' || color == 'warning' ? '400' : '500'} rounded-full bg-white mr-3">
+  let html = `<div class="flex items-center bg-${color}-${mode == 'info' || mode == 'warning' ? '400' : '500'} border-l-4 border-${color}-700 py-2 px-3 shadow-md mb-2 ${costumize.roundedClass} ${costumize.animation}"><div class="text-${color}-${color == 'info' || color == 'warning' ? '400' : '500'} rounded-full bg-white mr-3">
   ${icon}</div><div class="text-white max-w-xs ${costumize.textClass}">${message || 'tidak ada message'}</div></div>`
   toast.insertAdjacentHTML('beforeend', html);
-  setTimeout(() => toast.removeChild(toast.firstChild), costumize.delay || 3000);
+  setTimeout(() => toast.removeChild(toast.firstChild), costumize.duration || 3000);
 }
